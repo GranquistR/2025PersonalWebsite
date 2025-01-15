2768bc1b4b96745aef47c9d7b9654cb1fec243c8
RYAN GRANQUIST <rmg61103@gmail.com>
2025-01-15 16:02:53 -0600

    Set themeing and made header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,6 +1,29 @@
 <template>
-  <MainLogo />
+  <div class="flex justify-content-around flex-wrap my-3">
+    <div class="equal-width">
+      <MainLogo />
+    </div>
+    <div class="flex gap-3">
+      <Button label="Home" severity="secondary" variant="text" />
+      <Button label="About Me" severity="secondary" variant="text" />
+      <Button label="Projects" severity="secondary" variant="text" />
+      <Button label="Work Experience" severity="secondary" variant="text" />
+      <Button label="Tools" severity="secondary" variant="text" />
+      <Button label="Contact" severity="secondary" variant="text" />
+    </div>
+    <div class="equal-width">
+      <Button icon="pi pi-search" severity="secondary" variant="text" />
+      <Button icon="pi pi-bars" class="ml-2" />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import MainLogo from '../components/MainLogo.vue'
+import { Button } from 'primevue'
 </script>
+
+<style scoped>
+.equal-width {
+  width: 100px;
+}
+</style>
--- src/components/MainLogo.vue
+++ src/components/MainLogo.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="logo">RG</div>
+  <div class="flex flex-row align-items-center">
+    <div class="logo-text">
+      <div class="logo-offset">RG</div>
+    </div>
+    <div class="text ml-4">Portfolio</div>
+  </div>
 </template>
 
+<script setup lang="ts"></script>
+
 <style scoped>
-.logo {
-  font-size: 2rem;
+.logo-text {
+  font-size: 2.5rem;
   font-weight: bold;
-  color: white;
-  background-color: orange;
-  padding: 0.5rem;
+  color: var(--p-primary-color);
+  line-height: 1;
+  border-bottom: 5px solid var(--p-primary-color);
+}
+
+.text {
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: var(--p-primary-color);
 }
 </style>
