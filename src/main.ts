8e566add7796dbba885cf54c4904b4b9e2d340ff
RYAN GRANQUIST <rmg61103@gmail.com>
2025-01-15 14:51:44 -0600

    Added primevue and primeflex

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import './assets/main.css'
 
+//Vue 3
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+//PrimeVue
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css'
+import Aura from '@primevue/themes/aura'
+import ToastService from 'primevue/toastservice'
+import Tooltip from 'primevue/tooltip'
 
-app.use(router)
+//PrimeFlex
+import 'primeflex/primeflex.css'
 
-app.mount('#app')
+createApp(App)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: '.dark-mode-toggle',
+      },
+    },
+  })
+  .use(router)
+  .use(ToastService)
+  .directive('tooltip', Tooltip)
+  .mount('#app')
