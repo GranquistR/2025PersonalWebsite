33c4663322b525c7a948df0ad37c82f029b0b908
RYAN GRANQUIST <rmg61103@gmail.com>
2025-01-15 22:21:46 -0600

    Added links menu and responsiveness to header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="flex justify-content-around flex-wrap my-3">
-    <div class="equal-width">
+    <div>
       <MainLogo />
     </div>
-    <div class="flex gap-3">
-      <Button label="Home" severity="secondary" variant="text" />
-      <Button label="About Me" severity="secondary" variant="text" />
-      <Button label="Projects" severity="secondary" variant="text" />
-      <Button label="Work Experience" severity="secondary" variant="text" />
-      <Button label="Tools" severity="secondary" variant="text" />
-      <Button label="Contact" severity="secondary" variant="text" />
+    <div class="gap-3 hidden lg:flex xl:flex">
+      <Button v-for="link in links" :label="link.label" severity="secondary" variant="text" />
     </div>
-    <div class="equal-width">
+    <div>
       <Button icon="pi pi-search" severity="secondary" variant="text" />
-      <Button icon="pi pi-bars" class="ml-2" />
+      <Drawer v-model:visible="menuVisible" position="right" class="w-full lg:w-min xl:w-min">
+        <template #header>
+          <div class="w-full flex justify-content-center flex-wrap">
+            <MainLogo />
+          </div>
+        </template>
+        <div class="flex flex-column gap-3">
+          <Button v-for="link in links" :label="link.label" severity="secondary" />
+        </div>
+      </Drawer>
+      <Button icon="pi pi-bars" @click="menuVisible = true" />
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import MainLogo from '../components/MainLogo.vue'
 import { Button } from 'primevue'
-</script>
+import { Drawer } from 'primevue'
 
-<style scoped>
-.equal-width {
-  width: 100px;
-}
-</style>
+const menuVisible = ref(false)
+
+const links = ref([
+  { label: 'Home', url: '/' },
+  { label: 'About Me', url: '/about' },
+  { label: 'Projects', url: '/projects' },
+  { label: 'Work Experience', url: '/work-experience' },
+  { label: 'Tools', url: '/tools' },
+  { label: 'Contact', url: '/contact' },
+])
+</script>
